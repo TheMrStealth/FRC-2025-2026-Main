04042f8b94d4754116a716d40bf3e2392b5f289b
Marauders7500 <maraudersFRC7500@gmail.com>
2026-01-31 17:01:35 -0500

    more subsystems

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -1,13 +1,32 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class Launcher {
-    private final SparkMax motorL10, motorL11;
+    private final SparkMax motorL9, motorL10;
 
     public Launcher() {
-        motorL10 = new SparkMax(10, MotorType.kBrushless);
-        motorL11 = new SparkMax(11, MotorType.kBrushless);
+        motorL9 = new SparkMax(10, MotorType.kBrushless);
+        motorL10 = new SparkMax(11, MotorType.kBrushless);
+
+        SparkMaxConfig smConfig = new SparkMaxConfig();
+        smConfig.idleMode(IdleMode.kBrake);
+
+        motorL9.configure(smConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        motorL10.configure(smConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public void enable() {
+        motorL9.set(0.8);
+        motorL10.set(0.8);
+    }
+    public void disable() {
+        motorL9.stopMotor();
+        motorL10.stopMotor();
     }
 }
